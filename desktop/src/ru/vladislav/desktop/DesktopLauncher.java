a10b0159173e9ca3b52909145c3939c02bb50f9f
Vladislav <cheapsbl@gmail.com>
2018-01-27 04:38:56 +0300

    Initial commit

---
+++ desktop/src/ru/vladislav/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.vladislav.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.vladislav.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
